1e1c6427a7f62623055781cbc1207400be1bb7f2
scarlett-n <scarlett.noorman@monash.edu>
2026-06-03 17:43:16 +1000

    adding second page and more links

--- script.js
+++ script.js
@@ -1,87 +1,128 @@
 
+ const cuteLinks = [
+    ["https://www.instagram.com/reels/DYKi7JwqP_l/", "3D printed Zoetrope"],
+    ["https://www.instagram.com/reels/DXxjRJvvSeu/", "Maximalist Cyberdeck"],
+    ["https://www.instagram.com/reels/DXwGYRlCi9-/", "Tarot Card Reading Machine"],
+    ["https://www.instagram.com/reels/DXAQ_p-E4gb/", "Raspberry Pi Photobooth"],
+    ["https://www.instagram.com/reels/DW629QBo5Ml/", "Slot Machine For Cats"],
+    ["https://www.instagram.com/reels/DS3IERVkbWJ/", "3D printed Plants that are also Tools"],
+    ["https://www.instagram.com/reels/DR5zodLE-hI/", "Upcycling a Shirt With Sharms"],
+    ["https://www.instagram.com/reels/DWl0JOYDZ5q/", "Arduino Mp3 Player"],
+    ["https://www.instagram.com/reels/DWmTzerkaQ3/", "Plant Tamagochi"],
+    ["https://www.instagram.com/reels/DVgrFgClyzn/", "Jellycat Inspired Pomodoro Timer"],
+    ["https://www.instagram.com/reels/DSqFjG5ie1O/", "Wing Headset"],
+    ["https://www.instagram.com/reels/DWASo-QjO6b/", "Interactive Trinket Shelf"],
+    ["https://www.instagram.com/reels/DL4jx6huz9o/", "Bee Metronome"],
+    ["https://www.instagram.com/reels/DY0yfiMsrTt/", "Angry Plushie Useless Box"],
+    ["https://www.instagram.com/reels/DVrWZo6ifR9/", "Push Button Cute Messaging Devices"],
+    ["https://www.instagram.com/reels/DYtIT2WOT2O/", "Felt Fish"],
+    ["https://www.instagram.com/reels/DX8AuirBGzs/", "Bird Hat Mobile"],
+    ["https://www.instagram.com/reels/DXaAda6kgEa/", "Collage Art"],
+    ["https://www.instagram.com/reels/DYztVi6O5Hq/", "Mailbox Messenger"],
+    ["https://www.instagram.com/reels/DYj13-7uDpR/", "Kid Art Button"],
+    ["https://www.instagram.com/reels/DVUhCluEm4r/", "DIY Digital Camera"]
+];
+
+const arduinoLinks = [
+    ["https://www.instagram.com/reels/DYiBMM4oJSP/", "Desktop Radar with Live Plane Tracking"],
+    ["https://www.instagram.com/reels/DYzpngyxeF7/", "Playing Snake Using Guitar Chords"],
+    ["https://www.instagram.com/reels/DWfzs6riUU2/", "Playing Elden Ring using an Inhaler"],
+    ["https://www.instagram.com/reels/DXwGYRlCi9-/", "Tarot Card Reading Machine"],
+    ["https://www.instagram.com/reels/DXeTSqljAs-/", "Morse Code Decoder"],
+    ["https://www.instagram.com/reels/DWCYk5MkaWh/", "Machine that Smokes when Github has an Outage"],
+    ["https://www.instagram.com/reels/DXAQ_p-E4gb/", "Raspberry Pi Photobooth"],
+    ["https://www.instagram.com/reels/DXKby7JCnDl/", "Interactive Meme Cat"],
+    ["https://www.instagram.com/reels/DW629QBo5Ml/", "Slot Machine For Cats"],
+    ["https://www.instagram.com/reels/DSrVVMnDlcN/", "IRL Overcooked Game Made with Cardboard"],
+    ["https://www.instagram.com/reels/DGdiYxEJrkM/", "Device That Orders You a Random Meal at McDonalds"],
+    ["https://www.instagram.com/reels/DMiRxD3AFNA/", "Auto Aiming Rubbish Bin"],
+    ["https://www.instagram.com/reels/DWl0JOYDZ5q/", "Arduino Mp3 Player"],
+    ["https://www.instagram.com/reels/DWmTzerkaQ3/", "Plant Tamagochi"],
+    ["https://www.instagram.com/reels/DVgrFgClyzn/", "Jellycat Inspired Pomodoro Timer"],
+    ["https://www.instagram.com/reels/DSqFjG5ie1O/", "Wing Headset"],
+    ["https://www.instagram.com/reels/DWASo-QjO6b/", "Interactive Trinket Shelf"],
+    ["https://www.youtube.com/watch?v=N-uSTRGhEhs", "Haunted Portrait"],
+    ["https://www.youtube.com/watch?v=l5ggH-YhuAw", "BMO AI Agent"],
+    ["https://www.youtube.com/watch?v=GoMsm543W9Y", "Playing Elden Ring with a Banana Controller"],
+    ["https://www.youtube.com/watch?v=YmmPsDW58Oc", "A Device that Shocks People When They Touch Your Stuff"],
+    ["https://www.instagram.com/reels/DYX7d6RJXZo/", "Mini Weather Station"],
+    ["https://www.instagram.com/reels/DYVd7CLJjK2/", "Upcycled Alarm Clock"],
+    ["https://www.instagram.com/reels/DVrWZo6ifR9/", "Push Button Cute Messaging Devices"],
+    ["https://www.instagram.com/reels/DYC-d1oOqTw/", "Recieve a Divine Message"],
+    ["https://www.instagram.com/reels/DV_IXSLjfHP/", "ASCII Aquarium"],
+    ["https://www.instagram.com/reels/DYj13-7uDpR/", "Kid Art Button"],
+    ["https://www.instagram.com/reels/DXW4ZYHDNP2/", "Mini Speed Reading EReader"]
+    ["https://www.instagram.com/reels/DVUhCluEm4r/", "DIY Digital Camera"],
+    ["https://www.instagram.com/reels/DXVOMxMgU0q/", "Ferry Departure Times Display"],
+    ["https://www.instagram.com/reels/DXcxaSMEjqv/", "Hardcore Disco Ball"],
+    ["https://www.instagram.com/reels/DXcxaSMEjqv/", "NFC Product Stock Counter"],
+];
+
+const recycledLinks = [
+    ["https://www.instagram.com/reels/DVnJ499ibBH/", "Turning Old Towels Into Clothes"],
+    ["https://www.instagram.com/reels/DXAQ_p-E4gb/", "Placeholder Title"],
+    ["https://www.instagram.com/reels/DSrVVMnDlcN/", "IRL Overcooked Game Made with Cardboard"],
+    ["https://www.instagram.com/reels/DR5zodLE-hI/", "Raspberry Pi Photobooth"],
+    ["https://www.instagram.com/reels/DUZAaMpjyYQ/", "Pants Made of Ties"],
+    ["https://www.instagram.com/reels/DU-zg9SjIT3/", "Cyanotype Printing with Nature on Socks"],
+    ["https://www.instagram.com/reels/DYVd7CLJjK2/", "Upcycled Alarm Clock"],
+    ["https://www.instagram.com/reels/DYj13-7uDpR/", "Kid Art Button"]
+];
+
+const weirdLinks = [
+    ["https://www.instagram.com/reels/DYzpngyxeF7/", "Playing Snake Using Guitar Chords"],
+    ["https://www.instagram.com/reels/DYuWt-AoknH/", "Unconventionally Controlled Games"],
+    ["https://www.instagram.com/reels/DYlk7DWx8Ms/", "AR Swim Goggles"],
+    ["https://www.instagram.com/reels/DWfzs6riUU2/", "Playing Elden Ring using an Inhaler"],
+    ["https://www.instagram.com/reels/DXwGYRlCi9-/", "Tarot Card Reading Machine"],
+    ["https://www.instagram.com/reels/DWCYk5MkaWh/", "Machine that Smokes when Github has an Outage"],
+    ["https://www.instagram.com/reels/DVOsIibjflS/", "An Instrument Where You Place Counters to Play Notes"],
+    ["https://www.instagram.com/reels/DXKby7JCnDl/", "Interactive Meme Cat"],
+    ["https://www.instagram.com/reels/DQ43GxVCR0r/", "Automated Mini Bowling"],
+    ["https://www.instagram.com/reels/DSrVVMnDlcN/", "IRL Overcooked Game Made with Cardboard"],
+    ["https://www.instagram.com/reels/DVOvVjDDDvv/", "Warhammer but Make It Cute"],
+    ["https://www.instagram.com/reels/DGdiYxEJrkM/", "Device That Orders You a Random Meal at McDonalds"],
+    ["https://www.instagram.com/reels/DITts9SNiuF/", "Modular 3D printed Storage"],
+    ["https://www.instagram.com/reels/DMiRxD3AFNA/", "Auto Aiming Rubbish Bin"],
+    ["https://www.instagram.com/reels/DZFhjzrziYh/", "Crank to Make The Cat Do The Thing"],
+    ["https://www.instagram.com/reels/DV_IXSLjfHP/", "ASCII Aquarium"],
+    ["https://www.instagram.com/reels/DWxs7_8jUs4/", "Send Stuff to a Reciept Printer"],
+    ["https://www.instagram.com/reels/Mailbox Messenger/", "Placeholder Title"],
+    ["https://www.instagram.com/reels/DY7rjG7xTKr/", "Abandoned Projects Graveyard"],
+    ["https://www.youtube.com/shorts/xkR6BS0Sacc", "Inventive Light Swithces"],
+    ["https://www.instagram.com/reels/DXW4ZYHDNP2/", "Mini Speed Reading EReader"]
+];
+
+const cursedLinks = [
+    ["https://www.instagram.com/reels/DTYYxl4ku-_/", "Honk to Scroll Tiktok"],
+    ["https://www.instagram.com/p/DYwIXi-jWrL/?img_index=1", "Self Skateboarding Skateboard"],
+    ["https://www.instagram.com/reels/DXHcwMVDKYO/", "Self Desctructing bot"],
+    ["https://www.instagram.com/reels/DVoGvhlDrLa/", "Sit Up Straight or Else"],
+    ["https://www.youtube.com/watch?v=N-uSTRGhEhs", "Haunted Portrait"],
+    ["https://www.instagram.com/reels/DYzh1Y6BrN0/", "Lovely Nightlight"],
+    ["https://www.instagram.com/reels/DYC-d1oOqTw/", "Recieve a Divine Message"]
+];
+
+const accessibleLinks = [
+    ["https://www.instagram.com/reels/DYR4ttoBg2p/", "Switch Adapted Nerf Gun"],
+    ["https://www.instagram.com/reels/DXp6VhNik5p/", "Eye Transfer Frame for Card Games"],
+    ["https://www.instagram.com/reels/DXW4ZYHDNP2/", "Mini Speed Reading EReader"],
+    ["https://www.instagram.com/reels/DFoUzIsJ6y0/", "Budget Sip and Puff Controller"],
+    ["https://www.instagram.com/reels/Cn9X-DMgfWn/", "Adapted Nail Clipper Holder"],
+    ["https://www.instagram.com/reels/DYm9BBqqWv7/", "Hub to Use Standard Mice and Keyboard as Phone Inputs"],
+    ["https://www.instagram.com/reels/DXE-7xlCMgI/", "Switch-Scanning Auditory AAC Device"],
+    ["https://www.instagram.com/reels/DY7sH4MA1hl/", "Robotic Feeding System"],
+    ["https://www.instagram.com/reels/DXpXsYkgN8I/", "Device for Adaptive Drawing and Painting"]
+   
+];
+
 const establishLinks =  function() {
 
-    const cuteLinks = [
-        "https://www.instagram.com/p/DYKi7JwqP_l/",
-        "https://www.instagram.com/p/DXxjRJvvSeu/",
-        "https://www.instagram.com/p/DXwGYRlCi9-/",
-        "https://www.instagram.com/p/DXAQ_p-E4gb/",
-        "https://www.instagram.com/p/DW629QBo5Ml/",
-        "https://www.instagram.com/p/DS3IERVkbWJ/",
-        "https://www.instagram.com/p/DR5zodLE-hI/",
-        "https://www.instagram.com/reels/DWl0JOYDZ5q/",
-        "https://www.instagram.com/reels/DWmTzerkaQ3/",
-        "https://www.instagram.com/reels/DVgrFgClyzn/",
-        "https://www.instagram.com/reels/DSqFjG5ie1O/",
-        "https://www.instagram.com/reels/DWASo-QjO6b/",
-        "https://www.youtube.com/watch?v=zJU2s-gfBYo",
-        "https://www.instagram.com/reels/DL4jx6huz9o/"
-    ];
-    const arduinoLinks = [
-        "https://www.instagram.com/p/DYiBMM4oJSP/",
-        "https://www.instagram.com/p/DYzpngyxeF7/",
-        "https://www.instagram.com/p/DWfzs6riUU2/",
-        "https://www.instagram.com/p/DXwGYRlCi9-/",
-        "https://www.instagram.com/p/DXeTSqljAs-/",
-        "https://www.instagram.com/p/DWCYk5MkaWh/",
-        "https://www.instagram.com/p/DXAQ_p-E4gb/",
-        "https://www.instagram.com/p/DXKby7JCnDl/",
-        "https://www.instagram.com/p/DW629QBo5Ml/",
-        "https://www.instagram.com/p/DSrVVMnDlcN/",
-        "https://www.instagram.com/p/DGdiYxEJrkM/",
-        "https://www.instagram.com/p/DMiRxD3AFNA/",
-        "https://www.instagram.com/reels/DWl0JOYDZ5q/",
-        "https://www.instagram.com/reels/DWmTzerkaQ3/",
-        "https://www.instagram.com/reels/DVgrFgClyzn/",
-        "https://www.instagram.com/reels/DSqFjG5ie1O/",
-        "https://www.instagram.com/reels/DWASo-QjO6b/",
-        "https://www.youtube.com/watch?v=N-uSTRGhEhs",
-        "https://www.youtube.com/watch?v=EJtl-fRrP3k",
-        "https://www.youtube.com/watch?v=PKPuBuUgRZY",
-        "https://www.youtube.com/watch?v=l5ggH-YhuAw",
-        "https://www.youtube.com/watch?v=GoMsm543W9Y",
-        "https://www.youtube.com/watch?v=YmmPsDW58Oc"
-    ]
-    const recycledLinks = [
-        "https://www.instagram.com/p/DVnJ499ibBH/",
-        "https://www.instagram.com/p/DXAQ_p-E4gb/",
-        "https://www.instagram.com/p/DSrVVMnDlcN/",
-        "https://www.instagram.com/p/DR5zodLE-hI/",
-        "https://www.instagram.com/p/DUZAaMpjyYQ/"
-    ]
-    const weirdLinks = [
-        "https://www.instagram.com/p/DYzpngyxeF7/",
-        "https://www.instagram.com/p/DYuWt-AoknH/",
-        "https://www.instagram.com/p/DYlk7DWx8Ms/",
-        "https://www.instagram.com/p/DWfzs6riUU2/",
-        "https://www.instagram.com/p/DXwGYRlCi9-/",
-        "https://www.instagram.com/p/DWCYk5MkaWh/",
-        "https://www.instagram.com/p/DVOsIibjflS/",
-        "https://www.instagram.com/p/DXKby7JCnDl/",
-        "https://www.instagram.com/p/DQ43GxVCR0r/",
-        "https://www.instagram.com/p/DSrVVMnDlcN/",
-        "https://www.instagram.com/p/DVOvVjDDDvv/",
-        "https://www.instagram.com/p/DGdiYxEJrkM/",
-        "https://www.instagram.com/p/DITts9SNiuF/",
-        "https://www.instagram.com/p/DMiRxD3AFNA/"
-    ]
-    const cursedLinks = [
-        "https://www.instagram.com/p/DTYYxl4ku-_/",
-        "https://www.instagram.com/p/DYwIXi-jWrL/?img_index=1",
-        "https://www.instagram.com/p/DXHcwMVDKYO/",
-        "https://www.instagram.com/p/DVoGvhlDrLa/",
-        "https://www.youtube.com/watch?v=N-uSTRGhEhs"
-        
-    ]
-    const accessibleLinks = [
-        "https://www.instagram.com/p/DYR4ttoBg2p/",
-        "https://www.instagram.com/p/DXp6VhNik5p/"
-    ]
-
+  
     // 2. Pick a random index from the list
     var randomIndex = Math.floor(Math.random() * cursedLinks.length);
-    const cursedLink = cursedLinks[randomIndex];
-    console.log(randomIndex, cursedLink)
+    const cursedLink = cursedLinks[randomIndex][0];
+    
 
     // 3. Assign it to your HTML element (e.g., an <a> tag with id="random-link")
     var linkElement = document.getElementById("cursed");
@@ -91,7 +132,7 @@ const establishLinks =  function() {
 
 
     randomIndex = Math.floor(Math.random() * accessibleLinks.length);
-    const accessibleLink = accessibleLinks[randomIndex];
+    const accessibleLink = accessibleLinks[randomIndex][0];
 
      linkElement = document.getElementById("accessible");
     if (linkElement) {
@@ -99,7 +140,7 @@ const establishLinks =  function() {
     }
 
    randomIndex = Math.floor(Math.random() * arduinoLinks.length);
-    const arduinoLink = arduinoLinks[randomIndex];
+    const arduinoLink = arduinoLinks[randomIndex][0];
 
      linkElement = document.getElementById("arduino");
     if (linkElement) {
@@ -108,7 +149,7 @@ const establishLinks =  function() {
 
     
    randomIndex = Math.floor(Math.random() * recycledLinks.length);
-    const recycledLink = recycledLinks[randomIndex];
+    const recycledLink = recycledLinks[randomIndex][0];
 
      linkElement = document.getElementById("recycled");
     if (linkElement) {
@@ -116,7 +157,7 @@ const establishLinks =  function() {
     }
     
      randomIndex = Math.floor(Math.random() * weirdLinks.length);
-    const weirdLink = weirdLinks[randomIndex];
+    const weirdLink = weirdLinks[randomIndex][0];
 
      linkElement = document.getElementById("weird");
     if (linkElement) {
@@ -124,7 +165,7 @@ const establishLinks =  function() {
     }
 
        randomIndex = Math.floor(Math.random() * cuteLinks.length);
-    const cuteLink = cuteLinks[randomIndex];
+    const cuteLink = cuteLinks[randomIndex][0];
 
      linkElement = document.getElementById("cute");
     if (linkElement) {
@@ -189,3 +230,33 @@ function setupTitleImageSwap() {
 
 // initialize title image handler after load
 window.addEventListener('load', setupTitleImageSwap);
+
+
+//LIST PAGE
+
+var allTitles = "";
+const projectslistDiv = document.getElementById("projectslist");
+
+if (projectslistDiv) {
+    const allLinks = [
+        ...cuteLinks,
+        ...arduinoLinks,
+        ...recycledLinks,
+        ...weirdLinks,
+        ...cursedLinks,
+        ...accessibleLinks
+    ];
+
+    const titleSet = new Set();
+    for (const link of allLinks) {
+        const title = link[1];
+        if (titleSet.has(title)) {
+            continue;
+        }
+        titleSet.add(title);
+        allTitles += `<a href="${link[0]}"> ${title} &rarr;</a> <br>`;
+    }
+
+    // Overwrite content 
+    projectslistDiv.innerHTML = allTitles;
+}
